e6d7eb23a0487ef1442171a99a54747eeb353f5e
biud436 <biud436@gmail.com>
2022-10-26 18:42:32 +0900

    feat: 마지막 페이지에서 멈추는 버그가 있음. alert 버그가 있음

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const driver = new Builder()
         browserName: 'chrome',
         chromeOptions: {
             args: ['--headless', '--disable-gpu', '--window-size=1280,1024'],
-            position: '300,0',
+            position: '0,0',
         },
     })
     .build();
@@ -20,7 +20,7 @@ const driver = new Builder()
 /**
  * 수강중인 강의가 있는지 확인합니다.
  */
-//@ts-ignore
+// @ts-ignore
 async function isEnrolledLecture(_driver: ThenableWebDriver) {
     const target = '/images/usr/ip/cm/btn/btn_no_complete.gif';
 
@@ -37,234 +37,266 @@ async function isEnrolledLecture(_driver: ThenableWebDriver) {
     return isEnrolled;
 }
 
-(async () => {
-    await driver.get('https://general.ipacademy.net/servlet/MainController');
+// @ts-ignore
+async function alertWindowBlock(_driver: ThenableWebDriver) {
+    const alertWindow = await _driver.switchTo().alert();
+    await alertWindow.accept();
+}
 
-    await driver.wait(until.elementLocated(By.id('sso.ID')), 10000);
+// @ts-ignore
+async function safeFindElement(_driver: ThenableWebDriver, _selector: By) {
+    let element = null;
 
-    // sso.ID 요소 찾아서 입력
-    await driver.findElement(By.id('sso.ID')).sendKeys(process.env.ID);
-    // sso.PW 요소 찾아서 입력
-    await driver.findElement(By.id('sso.PW')).sendKeys(process.env.PW);
-    // 'btn_login' 클래스 요소 찾아서 클릭
-    await driver.findElement(By.className('btn_login')).click();
+    try {
+        element = await _driver.findElement(_selector);
+    } catch (e) {
+        console.log('safeFindElement', e);
+    }
 
-    // 잠시 기다린 후, 'https://general.ipacademy.net/servlet/MainController'로 이동
-    await driver.sleep(100);
-    await driver.get('https://general.ipacademy.net/servlet/MainController');
+    return element;
+}
 
-    // await driver.get(
-    //     'https://general.ipacademy.net/servlet/CourseController?cmd=1&sd=IP&md=IP_GEDU&ms=472&aspseq=1&CT=2&courseDivi=area'
-    // );
+class HostClient {
+    async start() {
+        await driver.get(
+            'https://general.ipacademy.net/servlet/MainController'
+        );
 
-    // // "/images/cm/subBtn/btn_lms_apply.gif" 라는 이미지를 가진 요소 중 3번째 요소를 찾아서 클릭
-    // await driver
-    //     .findElements(
-    //         By.xpath("//img[@src='/images/cm/subBtn/btn_lms_apply.gif']")
-    //     )
-    //     .then((elements) => elements[2].click());
+        await driver.wait(until.elementLocated(By.id('sso.ID')), 10000);
 
-    // // confirm이 뜨면 확인 버튼 클릭
-    // await driver.switchTo().alert().accept();
+        // sso.ID 요소 찾아서 입력
+        await driver.findElement(By.id('sso.ID')).sendKeys(process.env.ID);
+        // sso.PW 요소 찾아서 입력
+        await driver.findElement(By.id('sso.PW')).sendKeys(process.env.PW);
+        // 'btn_login' 클래스 요소 찾아서 클릭
+        await driver.findElement(By.className('btn_login')).click();
 
-    // // alert이 뜨면 확인 버튼 클릭
-    // await driver.switchTo().alert().accept();
+        // 잠시 기다린 후, 'https://general.ipacademy.net/servlet/MainController'로 이동
+        await driver.sleep(100);
+        await driver.get(
+            'https://general.ipacademy.net/servlet/MainController'
+        );
 
-    // 나의 수강실 'https://general.ipacademy.net/servlet/UsrKisuController?cmd=4&ms=466&sd=IP&CT=2&aspseq=1'로 이동
+        // await driver.get(
+        //     'https://general.ipacademy.net/servlet/CourseController?cmd=1&sd=IP&md=IP_GEDU&ms=472&aspseq=1&CT=2&courseDivi=area'
+        // );
 
-    await driver.get(
-        'https://general.ipacademy.net/servlet/UsrKisuController?cmd=4&ms=466&sd=IP&CT=2&aspseq=1'
-    );
+        // // "/images/cm/subBtn/btn_lms_apply.gif" 라는 이미지를 가진 요소 중 3번째 요소를 찾아서 클릭
+        // await driver
+        //     .findElements(
+        //         By.xpath("//img[@src='/images/cm/subBtn/btn_lms_apply.gif']")
+        //     )
+        //     .then((elements) => elements[2].click());
 
-    // document.querySelector(".subject > a") 를 클릭한다.
-    await driver
-        .findElement(By.css('.subject > a'))
-        .then((element) => element.click());
+        // // confirm이 뜨면 확인 버튼 클릭
+        // await driver.switchTo().alert().accept();
 
-    // 팝업 윈도우의 주소를 획득한다.
-    const popupWindow = await driver.getAllWindowHandles().then((handles) => {
-        return handles[1];
-    });
+        // // alert이 뜨면 확인 버튼 클릭
+        // await driver.switchTo().alert().accept();
 
-    // 팝업 윈도우로 이동한다.
-    await driver.switchTo().window(popupWindow);
+        // 나의 수강실 'https://general.ipacademy.net/servlet/UsrKisuController?cmd=4&ms=466&sd=IP&CT=2&aspseq=1'로 이동
 
-    // 팝업 윈도우가 열릴 때까지 기다린다.
-    await driver.wait(
-        until.elementLocated(By.css('div[name*="ItemList"]')),
-        10000 * 2
-    );
+        await driver.get(
+            'https://general.ipacademy.net/servlet/UsrKisuController?cmd=4&ms=466&sd=IP&CT=2&aspseq=1'
+        );
 
-    // 팝업 윈도우에서 By.css("div[name*='ItemList']") 요소의 td > a 요소를 찾아서 차례대로 클릭한다.
-    const itemList = await driver.findElement(By.css("div[name*='ItemList']"));
-
-    const elements = await itemList.findElements(By.css('td > a'));
-
-    // 3초 기다린다.
-    await driver.sleep(3000);
-
-    console.log(elements);
-
-    for (const element of elements) {
-        await driver.sleep(1000);
-
-        const elementText = await element.getText();
-
-        console.log(elementText + '체크 중...');
-
-        try {
-            // a의 부모인 td 선택하고 그 부모인 tr 선택
-            const tr = await element
-                .findElement(By.xpath('..'))
-                .findElement(By.xpath('..'));
-
-            console.log(`tr이 선택되었습니다 : ${await tr.getText()}`);
-
-            // 5번째 td의 텍스트를 찾는다
-            const td = await tr.findElement(By.xpath('td[5]'));
-
-            // 진행률 td 선택
-            const progressTd = td;
-
-            // 진행률 td의 텍스트를 가져온다.
-            const progressText = await progressTd.getText();
-
-            console.log(`진행률 : ${progressText}`);
-
-            if (progressText.includes('100%')) {
-                console.log(
-                    `${await element.getText()} 이미 수강 완료되었습니다.`
-                );
-                continue;
-            }
-
-            await element.click();
-
-            // 3초 기다린다.
-            await driver.sleep(3000);
-        } catch (e) {
-            console.warn(e);
-        }
-
-        // 마지막 팝업 윈도우에 포커스를 옮긴다.
-        const windows = await driver.wait(driver.getAllWindowHandles());
-        console.log(windows);
-        await driver.switchTo().window(windows[2]);
-
-        await driver.sleep(1000);
-        // iframe 찾기
-        // iframe으로 이동
+        // document.querySelector(".subject > a") 를 클릭한다.
         await driver
-            .switchTo()
-            .frame(driver.findElement(By.xpath(`\/\/*[@id="contents"]`)));
+            .findElement(By.css('.subject > a'))
+            .then((element) => element.click());
 
-        await driver.sleep(1000);
+        // 팝업 윈도우의 주소를 획득한다.
+        const popupWindow = await driver
+            .getAllWindowHandles()
+            .then((handles) => {
+                return handles[1];
+            });
 
-        const pageTotal = await driver
-            .findElement(By.className('page_total'))
-            .getText();
+        // 팝업 윈도우로 이동한다.
+        await driver.switchTo().window(popupWindow);
 
-        for (let i = 0; i < +pageTotal; i++) {
-            const pageCurrent = await driver
-                .findElement(By.className('page_current'))
-                .getText();
-            console.log(`%d / %d`, pageCurrent, pageTotal);
+        // 팝업 윈도우가 열릴 때까지 기다린다.
+        await driver.wait(
+            until.elementLocated(By.css('div[name*="ItemList"]')),
+            10000 * 2
+        );
+
+        // 팝업 윈도우에서 By.css("div[name*='ItemList']") 요소의 td > a 요소를 찾아서 차례대로 클릭한다.
+        const itemList = await driver.findElement(
+            By.css("div[name*='ItemList']")
+        );
+
+        const elements = await itemList.findElements(By.css('td > a'));
+
+        // 3초 기다린다.
+        await driver.sleep(3000);
+
+        for (const element of elements) {
             await driver.sleep(1000);
-            await driver.wait(
-                until.elementLocated(
-                    By.xpath(`\/\/*[@id="fs-footer"]/div/div[2]`)
-                ),
-                30000
-            );
 
-            // time_cur
-            // time_tol
+            const elementText = await element.getText();
 
-            let timeTol = await driver
-                .findElement(By.className('time_tol'))
-                .getText();
+            console.log(elementText + '체크 중...');
 
-            await driver.sleep(500);
+            try {
+                // a의 부모인 td 선택하고 그 부모인 tr 선택
+                const tr = await element
+                    .findElement(By.xpath('..'))
+                    .findElement(By.xpath('..'));
 
-            if (timeTol === '00:00') {
-                await driver.sleep(1000);
+                console.log(`tr이 선택되었습니다 : ${await tr.getText()}`);
+
+                // 5번째 td의 텍스트를 찾는다
+                const td = await tr.findElement(By.xpath('td[5]'));
+
+                // 진행률 td 선택
+                const progressTd = td;
+
+                // 진행률 td의 텍스트를 가져온다.
+                const progressText = await progressTd.getText();
+
+                console.log(`진행률 : ${progressText}`);
+
+                if (progressText.includes('100%')) {
+                    console.log(
+                        `${await element.getText()} 이미 수강 완료되었습니다.`
+                    );
+                    continue;
+                }
+
+                await element.click();
+
+                // 3초 기다린다.
+                await driver.sleep(3000);
+            } catch (e) {
+                console.warn(e);
             }
 
-            let page_current: any = 1;
+            // 마지막 팝업 윈도우에 포커스를 옮긴다.
+            const windows = await driver.wait(driver.getAllWindowHandles());
+            console.log(windows);
+            await driver.switchTo().window(windows[2]);
 
-            // For a webdriver.Condition or function, the wait will repeatedly evaluate the condition until it returns a truthy value.
-            // true가 나올때 까지 반복적으로 평가합니다.
-            let done = false;
-            while (!done) {
-                await driver.sleep(1500);
-                const timeCur = await driver
-                    .findElement(By.className('time_cur'))
+            await driver.sleep(1000);
+            // iframe 찾기
+            // iframe으로 이동
+            await driver
+                .switchTo()
+                .frame(driver.findElement(By.xpath(`\/\/*[@id="contents"]`)));
+
+            await driver.sleep(1000);
+
+            const pageTotal = await driver
+                .findElement(By.className('page_total'))
+                .getText();
+
+            for (let i = 0; i < +pageTotal; i++) {
+                const pageCurrent = await driver
+                    .findElement(By.className('page_current'))
                     .getText();
-                timeTol = await driver
+                console.log(`%d / %d`, pageCurrent, pageTotal);
+                await driver.sleep(1000);
+                await driver.wait(
+                    until.elementLocated(
+                        By.xpath(`\/\/*[@id="fs-footer"]/div/div[2]`)
+                    ),
+                    30000
+                );
+
+                // time_cur
+                // time_tol
+
+                let timeTol = await driver
                     .findElement(By.className('time_tol'))
                     .getText();
 
-                page_current = await driver
-                    .findElement(By.className('page_current'))
-                    .getText();
+                await driver.sleep(500);
 
-                const page_total = await driver
-                    .findElement(By.className('page_total'))
-                    .getText();
-
-                console.log(
-                    `현재 페이지 : ${page_current} / ${page_total} | 현재 시간 : ${timeCur} / ${timeTol}`
-                );
-
-                // TODO: 디버깅 용
-                // if (+page_current < +page_total) {
-                //     await driver.sleep(1000);
-                //     await driver.findElement(By.className('next')).click();
-                //     continue;
-                // }
-
-                if (timeCur === timeTol) {
+                if (timeTol === '00:00') {
                     await driver.sleep(1000);
+                }
 
-                    const btnNext = driver.findElement(
-                        By.className('next_tooltip')
+                let page_current: any = 1;
+
+                let done = false;
+                while (!done) {
+                    await driver.sleep(1500);
+                    const timeCur = await driver
+                        .findElement(By.className('time_cur'))
+                        .getText();
+                    timeTol = await driver
+                        .findElement(By.className('time_tol'))
+                        .getText();
+
+                    page_current = await driver
+                        .findElement(By.className('page_current'))
+                        .getText();
+
+                    const page_total = await driver
+                        .findElement(By.className('page_total'))
+                        .getText();
+
+                    console.log(
+                        `현재 페이지 : ${page_current} / ${page_total} | 현재 시간 : ${timeCur} / ${timeTol}`
                     );
 
-                    if (+page_current === +page_total) {
-                        console.log('마지막 페이지 입니다.');
-                        await driver.findElement(By.className('page_current'));
-                        done = true;
-                    }
+                    // TODO: 디버깅 용
+                    // if (+page_current < +page_total) {
+                    //     await driver.sleep(1000);
+                    //     await driver.findElement(By.className('next')).click();
+                    //     continue;
+                    // }
 
-                    if (+page_current === +page_total - 1) {
-                        await driver.findElement(By.className('next')).click();
-                    }
+                    if (timeCur === timeTol) {
+                        await driver.sleep(1000);
 
-                    if (await btnNext.isDisplayed()) {
-                        console.log(+page_current);
-                        console.log(+page_total);
-                        if (+page_current < +page_total) {
+                        const btnNext = driver.findElement(
+                            By.className('next_tooltip')
+                        );
+
+                        if (+page_current === +page_total) {
+                            console.log('마지막 페이지 입니다.');
+                            await driver.findElement(
+                                By.className('page_current')
+                            );
+                            done = true;
+                        }
+
+                        if (+page_current === +page_total - 1) {
                             await driver
                                 .findElement(By.className('next'))
                                 .click();
                         }
-                    } else {
-                        await driver.sleep(1000);
+
+                        if (await btnNext.isDisplayed()) {
+                            console.log(+page_current);
+                            console.log(+page_total);
+                            if (+page_current < +page_total) {
+                                await driver
+                                    .findElement(By.className('next'))
+                                    .click();
+                            }
+                        } else {
+                            await driver.sleep(1000);
+                        }
                     }
+                }
+
+                console.log('페이지 루프 종료');
+                console.log(`-- ${i} / ${pageTotal} --`);
+
+                if (+page_current === +pageTotal) {
+                    console.log('다른 강의로 이동합니다.');
+                    break;
                 }
             }
 
-            console.log('페이지 루프 종료');
-            console.log(`-- ${i} / ${pageTotal} --`);
-
-            if (+page_current === +pageTotal) {
-                console.log('다른 강의로 이동합니다.');
-                break;
-            }
+            await driver.sleep(3000);
+            await driver.close();
+            await driver.switchTo().window(windows[1]);
         }
-
-        await driver.sleep(3000);
-        await driver.close();
-        await driver.switchTo().window(windows[1]);
     }
-})();
+}
+
+const hostClient = new HostClient();
+hostClient.start();
